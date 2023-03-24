bc7e09bcf446a1faabc592240335cd8f55b3330d
adm.bjmn <adm.bjmn@gmail.com>
2023-03-24 16:47:08 +0000

    upload function creates CSV objects

--- book_site/config/upload/urls.py
+++ book_site/config/upload/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    # path('', views.upload_by_csv, name='upload'),
+    path('backup', views.backup_via_csv, name='backup'),
     path('', views.upload_by_scrape, name='upload'),
 ]
