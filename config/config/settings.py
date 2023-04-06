22d62f486c3cebedf44169ec5d5b01641441585d
adm.bjmn <adm.bjmn@gmail.com>
2023-04-06 17:31:15 +0100

    Deployment test 10-million & 10. changed file tree

--- config/config/settings.py
+++ config/config/settings.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import os
 from decouple import config
 import dj_database_url
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -126,3 +127,6 @@ MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_on_heroku.settings(locals())
